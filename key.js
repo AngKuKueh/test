e36697f46e4a528562213b1a04d8db7e2317252c
AngKuKueh <xiaohan027@gmail.com>
2026-07-04 23:59:06 +0800

    Add files via upload

---
+++ key.js
@@ -0,0 +1,16 @@
+// 這裡是你專屬的序號資料庫，以後只要改這個檔案就好！
+const sharedKeys = [
+    "WWMREDDIT0625",
+    "XRKPY6C38A",
+    "EKNAPHAHYY",
+    "WK7K8RY8JJ",
+    "QTNJP7P4PC",
+    "SY3KQQE8KR",
+    "TPCF3FEN6Y",
+    "NK4AJFPDWC",
+    "JJXDTKCD8Y",
+    "swcqqqhwae",
+    "KPC346C44A",
+    "PJCXEP4MFT",
+    "FHHXEYRJAN"
+];
